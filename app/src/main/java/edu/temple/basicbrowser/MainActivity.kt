fbfb0aa63042a435f60a30f9d4423569015eedc4
Brandon <tuk16746@temple.edu>
2023-11-06 15:56:16 -0500

    Created a validprotocol method that will check if the url is a valid url using URLUTIL.isValidURL, also previous commit message was inccorect, the user could type in wikipedia.com weihtout the https://

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -2,10 +2,13 @@ package edu.temple.basicbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,9 +32,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         goButton.setOnClickListener{
-            webView.loadUrl(urlEditText.text.toString())
+            val url = urlEditText.text.toString()
+            val parsedURL = validProtocol(url)
+            if(parsedURL) {
+                webView.loadUrl("https://$url")
+            }else{
+                Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
+            }
         }
 
+
+
+    }
+
+    private fun validProtocol(url: String): Boolean {
+        return URLUtil.isValidUrl("https://$url")
     }
 
 }
